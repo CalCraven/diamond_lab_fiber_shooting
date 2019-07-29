67fbadd7a4ab129224542caa707585fa33112fe9
Olivier <olgob@fysik.dtu.dk>
2019-07-29 17:04:21 +0200

    PEP 8 modifications

--- logic/fiber_shooting_logic.py
+++ logic/fiber_shooting_logic.py
@@ -23,11 +23,11 @@ from qtpy import QtCore
 
 from core.module import Base
 from interface.empty_interface import EmptyInterface
-from core.module import Connector, ConfigOption, StatusVar
+from core.module import Connector
 from core.util.mutex import Mutex
-import threading
 
-class fiber_shooting_logic(Base, EmptyInterface):
+
+class FiberShootingLogic(Base, EmptyInterface):
 
     """This is the Interface class to define the controls for the simple
     microwave hardware.
@@ -43,44 +43,48 @@ class fiber_shooting_logic(Base, EmptyInterface):
     sigPowerUpdated = QtCore.Signal()
     sigPowerDataNext = QtCore.Signal()
 
-    def on_activate(self):
-        """ Initialisation performed during activation of the module.
-        """
-        self._TiS_camera_hardware = self.get_connector('TiS_camera_hardware')
-        self._arduino_hardware = self.get_connector('arduino_hardware')
-        self._power_meter_hardware = self.get_connector('power_meter_hardware')
-
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+        self._TiS_camera_hardware = None
+        self._arduino_hardware = None
+        self._power_meter_hardware = None
         # Flipper variable
         self.flipper_opened = False
-
-        # Thread
-        self.threadlock = Mutex()
-        self.sigPowerDataNext.connect(self.set_power, QtCore.Qt.QueuedConnection)
-
         # Laser
         self.duty_cycle = 0
         self.frequency = 5000
-
         # PID
-        self.PID_status = False
+        self.pid_status = False
         self.ramp_status = False
         self.setpoint = 0.
         self.polarity = 1
-        self.Kp, self.Ki, self.Kd = 2, 1, 0.3
-        self.min_PID_out, self.max_PID_out = 0., 0.4
+        self.kp, self.ki, self.kd = 2, 1, 0.3
+        self.min_pid_out, self.max_pid_out = 0., 0.4
         self.ramping_factor = 0.01
         self.offset = 0
-        self.error_P_prev = 0.
-        self.error_P = 0
-        self.error_I = 0
-        self.error_D = 0
+        self.error_p_prev = 0.
+        self.error_p = 0
+        self.error_i = 0
+        self.error_d = 0
         self.output = 0
         self.time_loop = []
+        self.power = None
+        self.error = None
+
+        # Thread
+        self.threadlock = Mutex()
+
+    def on_activate(self):
+        """ Initialisation performed during activation of the module. """
+        self._TiS_camera_hardware = self.TiS_camera_hardware()
+        self._arduino_hardware = self.arduino_hardware()
+        self._power_meter_hardware = self.power_meter_hardware()
+
+        self.sigPowerDataNext.connect(self.set_power, QtCore.Qt.QueuedConnection)
         return
 
-
-
     def on_deactivate(self):
+        """  Performed during deactivation of the module. """
         self._TiS_camera_hardware.on_deactivate()
         self._arduino_hardware.on_desactivate()
         self._power_meter_hardware.on_desactivate()
@@ -92,132 +96,182 @@ class fiber_shooting_logic(Base, EmptyInterface):
         """ Resets the hardware, so the connection is lost and other programs can access it.
         """
         self.on_desactivate()
-        pass
 
-#################################################### Camera ############################################################
+    # Camera
 
     def setup_camera(self):
+        """ Setup the camera parameters. """
         self._TiS_camera_hardware.setup_camera()
+        return
 
     def start_video(self):
+        """ Start to capture camera frames. """
         self._TiS_camera_hardware.start_video_thread()
+        return
 
     def stop_video(self):
+        """ Stop to capture camera frames. """
         self._TiS_camera_hardware.set_video_status(False)
+        return
 
-    def get_cross_value(self):
-        return self._TiS_camera_hardware.get_cross_value()
+    def is_cross(self):
+        """ Return boolean to see if target cross is drawn or not on the camera streaming """
+        return self._TiS_camera_hardware.is_cross()
 
-    def get_cladding_value(self):
-        return self._TiS_camera_hardware.get_cladding_value()
+    def is_cladding(self):
+        """ Return boolean to see if the cladding of the fiber is drawn or not on the camera streaming """
+        return self._TiS_camera_hardware.is_cladding()
 
-    def get_core_value(self):
-        return self._TiS_camera_hardware.get_core_value()
+    def is_core(self):
+        """ Return boolean to see if the core of the fiber is drawn or not on the camera streaming """
+        return self._TiS_camera_hardware.is_core()
 
-    def get_jacket_value(self):
-        return self._TiS_camera_hardware.get_core_value()
+    def is_jacket(self):
+        """ Return boolean to see if the jacket of the fiber is drawn or not on the camera streaming """
+        return self._TiS_camera_hardware.is_core()
 
-    def set_cross_value(self, value):
-        self._TiS_camera_hardware.set_cross_value(value)
+    def set_cross(self, boolean):
+        """ Set the cross drawn /not drawn on the camera video. """
+        self._TiS_camera_hardware.set_cross(boolean)
+        return
 
-    def set_jacket_value(self, value):
-        self._TiS_camera_hardware.set_jacket_value(value)
+    def set_jacket(self, boolean):
+        """ Set the jacket of the fiber drawn /not drawn on the camera video. """
+        self._TiS_camera_hardware.set_jacket(boolean)
+        return
 
-    def set_cladding_value(self, value):
-        self._TiS_camera_hardware.set_cladding_value(value)
+    def set_cladding(self, boolean):
+        """ Set the cladding of the fiber drawn /not drawn on the camera video. """
+        self._TiS_camera_hardware.set_cladding(boolean)
+        return
 
-    def set_core_value(self, value):
-        self._TiS_camera_hardware.set_core_value(value)
+    def set_core(self, boolean):
+        """ Set the core of the fiber drawn /not drawn on the camera video. """
+        self._TiS_camera_hardware.set_core(boolean)
+        return
 
-    def set_edge_detection_value(self, value):
-        self._TiS_camera_hardware.set_edge_detection_value(value)
+    def set_edge_detection(self, value):
+        """ Set the edge detection threshold on the camera video. """
+        self._TiS_camera_hardware.set_edge_detection(value)
+        return
+
+    def is_edge_detection(self):
+        """ Get the edge detection status the camera video. """
+        return self._TiS_camera_hardware.is_edge_detection()
 
     def set_zoom_factor(self, value):
+        """ Set the scale factor of the drawings on the video frames. """
         self._TiS_camera_hardware.set_zoom_factor(value)
+        return
 
     def get_zoom_factor(self):
+        """ Get the scale factor of the drawings on the video frames. """
         return self._TiS_camera_hardware.get_zoom_factor()
 
     def set_edge_min(self, value):
+        """ Set the edge detection minimum threshold on the camera video. """
         self._TiS_camera_hardware.set_edge_min(value)
+        return
 
     def get_edge_min(self):
+        """ Get the edge detection minimum threshold on the camera video. """
         return self._TiS_camera_hardware.get_edge_min()
 
     def set_edge_max(self, value):
+        """ Set the edge detection maximum threshold on the camera video. """
         self._TiS_camera_hardware.set_edge_max(value)
+        return
 
     def get_edge_max(self):
+        """ Get the edge detection maximum threshold on the camera video. """
         return self._TiS_camera_hardware.get_edge_max()
 
-    def get_edge_detection_value(self):
-        return self._TiS_camera_hardware.get_edge_detection_value()
-
-#################################################### Flipper ###########################################################
+    # Flipper
 
     def open_flipper(self):
+        """ Open the flipper. """
         self._arduino_hardware.toggle_shutter(0)
+        return
 
-#################################################### Shutter ###########################################################
+    # Shutter
 
     def open_shutter(self):
+        """ Open the shutter (permanently). """
         self._arduino_hardware.toggle_shutter(1)
+        return
 
     def send_pulse(self, duration):
+        """ Open/close the shutter with a certain duration (min : about 6 ms). """
         self._arduino_hardware.open_shutter_micro(1, int(duration*1e3))
+        return
 
-################################################## CO2 Laser ###########################################################
+    # CO2 Laser
 
     def set_duty_cycle(self, duty_cycle):
+        """ Set the duty cycle of the laser (between 0 and 1). """
         self.duty_cycle = duty_cycle
-        self._arduino_hardware.setduty(self.duty_cycle)
+        self._arduino_hardware.set_duty(self.duty_cycle)
 
     def get_duty_cycle(self):
+        """ Get the duty cycle of the laser. """
         return self.duty_cycle
 
     def set_frequency(self, frequency):
+        """ Set the frequency of the laser (kHz). """
         self.frequency = frequency
-        self._arduino_hardware.setfreq(self.frequency)
+        self._arduino_hardware.set_freq(self.frequency)
+        return
 
     def get_frequency(self):
+        """ Get the frequency of the laser"""
         return self.frequency
 
     def set_setpoint(self, setpoint):
+        """ Set the power setpoint of the laser. """
         self.setpoint = setpoint
+        return
 
     def get_setpoint(self):
+        """ Get the power setpoint of the laser. """
         return self.setpoint
 
-    def set_PID_status(self, bool):
-        if bool == True:
-            self.PID_status = True
-        else:
-            self.PID_status = False
+    def set_pid_status(self, boolean):
+        """ Set the PID to True or False. """
+        self.pid_status = boolean
+        return
 
-    def get_PID_status(self):
-        return self.PID_status
+    def is_pid_status(self):
+        """ Get the status of the PID (boolean). """
+        return self.pid_status
 
-    def set_Kp(self, value):
-        self.Kp = value
+    def set_kp(self, value):
+        """ Set the proportional term of the PID. """
+        self.kp = value
+        return
 
-    def set_Ki(self, value):
-        self.Ki = value
+    def set_ki(self, value):
+        """ Set the integral term of the PID. """
+        self.ki = value
+        return
 
-    def set_Kd(self, value):
-        self.Kd = value
+    def set_kd(self, value):
+        """ Set the derivative term of the PID. """
+        self.kd = value
+        return
 
-    def clear_I(self):
-        self.error_I = 0
+    def clear_integral(self):
+        """ Clear the integral term of the PID. """
+        self.error_i = 0
+        return
 
-    def set_ramp_status(self,bool):
-        if bool == True:
-            self.ramp_status = True
-        else:
-            self.ramp_status = False
+    def set_ramp_status(self, boolean):
+        """ Set the ramp status (True ot False).
+        Used in order to increase the changes of laser power"""
+        self.ramp_status = boolean
+        return
 
     def set_power(self):
-        '''Set the duty cycle with or without PID'''
-        # with self.threadlock:
+        """Set the duty cycle with or without PID"""
         # measure power an the time
         self.power = self.get_power()
         self.time_loop.append(time.time())
@@ -231,42 +285,41 @@ class fiber_shooting_logic(Base, EmptyInterface):
                 # We update the power on the GUI
                 self.sigPowerUpdated.emit()
                 self.error = self.get_setpoint() - self.power
-                if self.ramp_status == True:
+                if self.ramp_status:
                     if abs(self.error) > 1e-3:
                         self.offset = self.output
                         self.output += np.sign(self.error)*1e-4
                         self.set_duty_cycle(self.output)
                     else:
                         self.ramp_status = False
-                        self.clear_I
-                        self.PID_status = True
-                elif self.PID_status == True:
+                        self.clear_integral()
+                        self.pid_status = True
+                elif self.pid_status:
                     delta_t = self.time_loop[-1] - self.time_loop[-2]
-                    self.error_P = self.error
-                    self.error_I += self.error * delta_t
-                    self.error_D = (self.error - self.error_P_prev) / delta_t
-                    P = self.Kp * self.error_P
-                    I = self.Ki * self.error_I
-                    D = self.Kd * self.error_D
-                    PID_out = self.polarity * (P + I + D/100)
-                    correction = self.offset + PID_out
-                    if correction >= self.max_PID_out:
-                        self.output = self.max_PID_out
-                    elif correction <= self.min_PID_out:
-                        self.output = self.min_PID_out
+                    self.error_p = self.error
+                    self.error_i += self.error * delta_t
+                    self.error_d = (self.error - self.error_p_prev) / delta_t
+                    p = self.kp * self.error_p
+                    i = self.ki * self.error_i
+                    d = self.kd * self.error_d
+                    pid_out = self.polarity * (p + i + d/100)
+                    correction = self.offset + pid_out
+                    if correction >= self.max_pid_out:
+                        self.output = self.max_pid_out
+                    elif correction <= self.min_pid_out:
+                        self.output = self.min_pid_out
                     else:
                         self.output = correction
                     self.set_duty_cycle(self.output)
+                    self.error_p_prev = self.error_p
                 else:
                     self.set_duty_cycle(self.duty_cycle)
         self.sigPowerDataNext.emit()
+        return
 
-################################################## Power Meter #########################################################
+    # Power Meter
 
     def get_power(self):
+        """ Get the optical power measured by the power meter. """
         power_data = self._power_meter_hardware.power_meter.read
         return power_data
-
-
-
-
